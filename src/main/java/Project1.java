6534ba4a5db201f9843dba488c049b5fc923427c
MarkKoz <KozlovMark@gmail.com>
2021-10-07 12:07:32 -0700

    Implement input preprocessing

--- src/main/java/Project1.java
+++ src/main/java/Project1.java
@@ -1,7 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Project1
 {
-    public static void main(final String[] args)
+    public static void main(final String[] args) throws IOException
     {
-        System.out.println("Hello, world!");
+        final Path file = Paths.get("input.txt");
+        final List<String> lines = Files.readAllLines(file);
+
+        final int n = Integer.parseInt(lines.get(0));
+        final String[] preferences = new String[n];
+        final Iterable<String> linesIterable = lines.stream().skip(1)::iterator;
+
+        for (final String line : linesIterable) {
+            int manRank = 1;
+
+            for (final String num : line.split(" ")) {
+                final int manIndex = Integer.parseInt(num) - 1;
+                preferences[manIndex] = String.valueOf(manRank);
+                ++manRank;
+            }
+
+            System.out.println(String.join("\t", preferences));
+        }
     }
 }
