a1eba357f5d344ba097fb8994b8524a1233f0deb
MarkKoz <KozlovMark@gmail.com>
2021-11-01 21:35:15 -0700

    Use the stream API instead of a record object

--- src/main/java/Project2.java
+++ src/main/java/Project2.java
@@ -5,58 +5,59 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Project2
 {
-    public record Rating(int rating, int position) implements Comparable<Rating>
-    {
-        @Override
-        public int compareTo(final Rating other)
-        {
-            return Integer.compare(rating, other.rating);
-        }
-    }
-
     public static void main(final String[] args) throws IOException
     {
-        final Rating[] ratings = readRatings();
-        Arrays.sort(ratings);
+        final int[] ratings = readRatings();
+
+        // clang-format off
+
+        // Sort the indices based on the ratings.
+        final Stream<Integer> positions = IntStream.range(0, ratings.length)
+          .boxed()
+          .sorted(Comparator.comparingInt(i -> ratings[i]));
+
+        // clang-format on
 
         // Final values must be > 1, so the default of 0 is a perfect "empty" value.
         final int[] out = new int[ratings.length];
         int total = 0;
 
-        for (final Rating rating : ratings) {
+        for (final int position : (Iterable<Integer>) positions::iterator) {
             // Clamp the indices to the bounds of the array.
             // If the neighbour is out of bounds, it will just stay at the current
             // position, which has a value of 0 and is therefore equivalent to
             // it having a neighbour there that was empty.
-            final int left = out[max(rating.position - 1, 0)];
-            final int right = out[min(rating.position + 1, out.length - 1)];
+            final int left = out[max(position - 1, 0)];
+            final int right = out[min(position + 1, out.length - 1)];
 
             // To minimise the value being inserted, find the greater neighbour and
             // add 1 to it. Why +1? Because the list is sorted in ascending order.
             // Therefore, the current item will always be larger than its neighbours.
             // Thus, the resulting value must also be larger than its neighbours.
-            out[rating.position] = max(left, right) + 1;
+            out[position] = max(left, right) + 1;
 
             // Track the total now to avoid an extra iteration later to sum it up.
-            total += out[rating.position];
+            total += out[position];
         }
 
         System.out.println(total);
     }
 
-    private static Rating[] readRatings() throws IOException
+    private static int[] readRatings() throws IOException
     {
         final Path file = Paths.get("input.txt");
         final List<String> lines = Files.readAllLines(file);
 
         // Get the first line and use it to allocate an array of that size.
         final int n = Integer.parseInt(lines.get(0));
-        final Rating[] ratings = new Rating[n];
+        final int[] ratings = new int[n];
 
         // Create an iterator of lines that skips the 1st line, since that was already read above.
         final Iterable<String> linesIterable = lines.stream().skip(1)::iterator;
@@ -67,7 +68,7 @@ public class Project2
         for (final String line : linesIterable) {
             // Split each line by any whitespace chars.
             for (final String num : line.split("\\s+")) {
-                ratings[position] = new Rating(Integer.parseInt(num), position);
+                ratings[position] = Integer.parseInt(num);
                 ++position;
             }
         }
