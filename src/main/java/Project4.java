5576c58fa3002fedc702cbdc2d546f47ac23b56d
MarkKoz <KozlovMark@gmail.com>
2021-11-29 08:19:58 -0800

    Read the input file into an array

--- src/main/java/Project4.java
+++ src/main/java/Project4.java
@@ -1,7 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Project4
 {
-    public static void main(final String[] args)
+    public static void main(final String[] args) throws IOException
     {
-        return;
+        final Integer[] integers = readIntegers();
+    }
+
+    private static Integer[] readIntegers() throws IOException
+    {
+        final Path file = Paths.get("input.txt");
+        final String content = Files.readString(file);
+        final Matcher matcher = Pattern.compile("-?\\d+").matcher(content);
+
+        // Skip the 1st item (length) because it's redundant with streams.
+        return matcher.results()
+            .skip(1)
+            .map(MatchResult::group)
+            .map(Integer::parseInt)
+            .toArray(Integer[] ::new);
     }
 }
